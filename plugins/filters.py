9759c6c8431d84f7fc9857bb70bdd6fc09e7d33d
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2023-06-20 17:40:08 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -13,7 +13,7 @@ from utils import get_file_id, parser, split_quotes
 from info import ADMINS
 
 
-@Client.on_message(filters.command(['filter', 'add']) & filters.incoming)
+@Client.on_message(filters.command(['zilter', 'addzilter']) & filters.incoming)
 async def addfilter(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
@@ -116,7 +116,7 @@ async def addfilter(client, message):
     )
 
 
-@Client.on_message(filters.command(['viewfilters', 'filters']) & filters.incoming)
+@Client.on_message(filters.command(['viewzilters', 'zilters']) & filters.incoming)
 async def get_all(client, message):
     
     chat_type = message.chat.type
