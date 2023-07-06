d5c25d7db812e273ec291d1fd89fa4eaf31d8655
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2023-07-06 12:53:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -665,10 +665,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
 async def auto_filter(client, msg, spoll=False):
 
-    await message.reply_chat_action(enums.ChatAction.TYPING)
-        mz=await message.reply_sticker("CAACAgQAAxkBAAEJm91kpmkN5jA8J4hUgaCv_HLlOSgntgAC7g0AAn2mwVC98nx2ic38Gi8E")
-        await asyncio.sleep(1)
-        await mz.delete()
+    
 
 
     if not spoll:
@@ -788,6 +785,10 @@ async def auto_filter(client, msg, spoll=False):
             await asyncio.sleep(IMDB_DELET_TIME)
             await fek.delete()
     else:
+        await message.reply_chat_action(enums.ChatAction.TYPING)
+        mz=await message.reply_sticker("CAACAgQAAxkBAAEJm91kpmkN5jA8J4hUgaCv_HLlOSgntgAC7g0AAn2mwVC98nx2ic38Gi8E")
+        await asyncio.sleep(1)
+        await mz.delete()
         fuk = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
         await asyncio.sleep(IMDB_DELET_TIME)
         await fuk.delete()        
