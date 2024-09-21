65c642b5912833b880fb1d81a8f1cc113943e199
MAULLIKPATEL <119292297+MAULLIKPATEL@users.noreply.github.com>
2024-09-22 01:50:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -685,7 +685,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=await get_shortilink(await get_shortlink(f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=await get_shortilink(await get_shortlink(f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}"))
                 ),
             ]
             for file in files
@@ -694,12 +694,10 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}",
-                    url=await get_shortilink(await get_shortlink(f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=await get_shortilink(await get_shortlink(f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}"))
                 ),
-                InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
-                    url=await get_shortilink(await get_shortlink(f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
+               InlineKeyboardButton(
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=await get_shortilink(await get_shortlink(f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}"))
                 ),
             ]
             for file in files
