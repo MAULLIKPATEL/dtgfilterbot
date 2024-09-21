27052bdd35413375e0e620ab50f63a8a1c9df0ff
MAULLIKPATEL <119292297+MAULLIKPATEL@users.noreply.github.com>
2024-09-22 00:25:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,6 +48,11 @@ IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌�
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+#first and second
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'myapi')
+SHORTILINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
+SHORTILINK_API = environ.get('SHORTLINK_API', 'myapi')
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
