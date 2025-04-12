b457d2c5c4bea4302c0ca46017e6e6e71f385260
MAULLIKPATEL <119292297+MAULLIKPATEL@users.noreply.github.com>
2025-04-12 19:08:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,5 +64,5 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-SHORTENER_API = environ.get("SHORTENER_API", 'b5cb953867c1a5efa2878d39a29b9f504cb435bc')
+SHORTENER_API = environ.get("SHORTENER_API", '681bf6ed2bdc16d93cdc6842be62a1baf6364e82')
 LONG_DROPLINK_URL = environ.get("LONG_DROPLINK_URL", False)
